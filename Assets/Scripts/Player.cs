1b00d04bd6eb59e4f8e9cf47954c49313bef9e38
KaffeFilter-collab <148773655+KaffeFilter-collab@users.noreply.github.com>
2023-12-01 11:17:18 +0100

    Camera area closed off

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
-     public float speed = 0;
-     public Vector2 input;
-     public float HP;
-     public delegate void playerdeath();
-     public event playerdeath Onplayerdeath;
-     public int damage;
-    public bool schlagen;
+    public float speed = 0;
+    public Vector2 input;
+    public float HP;
+    public delegate void playerdeath();
+    public event playerdeath Onplayerdeath;
+    public int damage;
     public int MaxHP = 10;
 
     private void Awake()
@@ -22,45 +21,43 @@ public class Player : MonoBehaviour
     }
     private void Update()
     {
-        schlagen = Input.GetKeyDown("mouse 1");
+
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Vertical");
     }
     private void FixedUpdate()
     {
         GetComponent<Rigidbody2D>().velocity = input * speed;
-        if (schlagen == true) Console.WriteLine("true"); ;
+
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<Collider2D>().gameObject.CompareTag("enemy"))
-        {
-            HP--;
-            if (HP >= 0)
-            {
-                Onplayerdeath?.Invoke();
-            
-            }
-            
-           
+        if (collision.GetComponent<Collider2D>().gameObject.CompareTag("wall"))
+       {
+            rigidbody2d.velocity = new Vector2(-rigidbody2d.velocity.x, -rigidbody2d.velocity.y);
+
         }
-        
+
+
     }
+
+       
     public void Heal(int healingAmount)
     {
 
-        if (HP+healingAmount <= MaxHP)
+        if (HP + healingAmount <= MaxHP)
         {
 
-        HP = HP + healingAmount;
+            HP = HP + healingAmount;
         }
-        else {
+        else
+        {
             HP = MaxHP;
         }
     }
 
 
-    
+
 
 }
