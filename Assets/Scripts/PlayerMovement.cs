6b3fbb1cbde4a666786e412b69c40b5cd1603ae1
Magnus Cochems <magnus@cochems.net>
2023-12-01 10:05:57 +0100

    rename player script
    "PlayerMovment" -Robin

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovment : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
      public float speed = 0;
