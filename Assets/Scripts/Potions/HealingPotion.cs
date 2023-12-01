efe29dfdac1dfa16a80abebb324c87e25c663c10
Magnus Cochems <magnus@cochems.net>
2023-12-01 10:46:29 +0100

    added potion spawning and healing

---
+++ Assets/Scripts/Potions/HealingPotion.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingPotion : MonoBehaviour {
+    [SerializeField]
+    private int healingAmount;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("player")) {
+            collision.gameObject.GetComponent<Player>().Heal(healingAmount);
+            Destroy(gameObject);
+        }
+    }
+}
