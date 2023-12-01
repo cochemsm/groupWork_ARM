6576443adf8e781486352aad9cf5654cf82a34ac
LiveDierepeater <81476895+LiveDierepeater@users.noreply.github.com>
2023-12-01 10:24:27 +0100

    Added Ghost
    Added Functions:
    - GhostMovement (Vector3 targetPosition)

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -1,8 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
-    
+    private GameManager gameManager;
+    private GameObject player;
+    private Rigidbody2D rigidbody2D;
+
+    public float movementSpeed = 3f;
+    public int hitPoints = 2;
+    public int attackDamage = 1;
+
+    private void Awake()
+    {
+        //gameManager = GameObject.Find("GameManager").GetComponentInChildren<GameManager>();
+        //player = gameManager.player;
+        player = GameObject.Find("Player");
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        GhostMovement(player.transform.position);
+    }
+
+    private void GhostMovement(Vector3 targetPosition)
+    {
+        Vector2 moveDirection =
+            new Vector2(targetPosition.x - transform.position.x, 
+                        targetPosition.y - transform.position.y);
+        moveDirection = moveDirection.normalized;
+        print(moveDirection);
+
+        rigidbody2D.velocity = moveDirection * movementSpeed;
+    }
 }
