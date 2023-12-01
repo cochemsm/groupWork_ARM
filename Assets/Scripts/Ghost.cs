0aba4a001dc9ca45787e40abbd1fc519062404c0
LiveDierepeater <81476895+LiveDierepeater@users.noreply.github.com>
2023-12-01 12:04:43 +0100

    Ghost now has TickRate and deals damage every second when in player collision box

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -1,11 +1,13 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Enemy))]
 public class Ghost : MonoBehaviour
 {
     private GameManager gameManager;
-    private GameObject player;
+    private Player player;
     private new Rigidbody2D rigidbody2D;
     private Enemy enemy;
 
@@ -13,11 +15,16 @@ public class Ghost : MonoBehaviour
     public int hitPoints = 2;
     public int attackDamage = 1;
 
+    // In which frame should the action be done in FixedUpdate.
+    public int attackTick = 50;
+    private int currentTick = 0;
+    private bool isAttacking = false;
+    
     private void Awake()
     {
         //gameManager = GameObject.Find("GameManager").GetComponentInChildren<GameManager>();
         //player = gameManager.player;
-        player = GameObject.Find("Player");
+        player = GameObject.Find("Player").GetComponent<Player>();
         rigidbody2D = GetComponent<Rigidbody2D>();
         enemy = GetComponent<Enemy>();
     }
@@ -27,13 +34,30 @@ public class Ghost : MonoBehaviour
         GhostMovement(player.transform.position);
     }
 
+    private void FixedUpdate()
+    {
+        currentTick++;
+        if (currentTick == attackTick)
+        {
+            HandleTick();
+            currentTick = 0;
+        }
+        print(isAttacking);
+        print(currentTick);
+    }
+    
+    private void HandleTick()
+    {
+        isAttacking = false;
+        print("Tick!");
+    }
+
     private void GhostMovement(Vector3 targetPosition)
     {
         Vector2 moveDirection =
             new Vector2(targetPosition.x - transform.position.x, 
                         targetPosition.y - transform.position.y);
         moveDirection = moveDirection.normalized;
-        print(moveDirection);
 
         rigidbody2D.velocity = moveDirection * movementSpeed;
     }
@@ -42,6 +66,16 @@ public class Ghost : MonoBehaviour
     {
         hitPoints -= damageAmount;
         if (hitPoints <= 0)
-            enemy.StartEvent();
+            enemy.StartEvent(); Destroy(this.gameObject);
+    }
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (isAttacking) return;
+            print("Player takes Damage");
+            isAttacking = true;
+        }
     }
 }
