9d941572c848ae527b6577e1fd8b881a73ec26ae
Magnus Cochems <magnus@cochems.net>
2023-12-01 10:04:17 +0100

    added game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private static GameManager instance;
+    public static GameManager Instance => instance;
+
+    public PlayerMovement player;
+    public GameObject[] enemys;
+
+    private void Awake() {
+        if (instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start() {
+        if (GameObject.FindGameObjectWithTag("player")) {
+            player = GameObject.FindGameObjectsWithTag("player");
+        }
+    }
+
+    private void Update() {
+        if (GameObject.FindGameObjectsWithTag("enemy") != null) {
+            enemys = GameObject.FindGameObjectsWithTag("enemy");
+        }
+    }
+}
